22f9bc6da43dbafcd39036ebad23dbd58c82ff64
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-28 23:56:39 +0200

    Added very basic styling. Added a user card component (intro). Added simple middleware.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,15 +2,21 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <Link href={"/"}>LOGO</Link>
-        <menu>
+    <header className="flex justify-center">
+      <nav className="container flex justify-between">
+        <Link href={"/"} className=".logo">
+          LOGO
+        </Link>
+        <menu className="flex gap-2">
           <li>
-            <Link href={"/api/auth/signin"}>Log In</Link>
+            <Link href={"/api/auth/signin"} className="navLink">
+              Log In
+            </Link>
           </li>
           <li>
-            <Link href={"/api/auth/signout"}>Log Out</Link>
+            <Link href={"/api/auth/signout"} className="navLink">
+              Log Out
+            </Link>
           </li>
         </menu>
       </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import usePrisma from "@/lib/hooks/usePrisma";
 import TaskList from "./components/TaskList";
+import { getServerSession } from "next-auth";
+import options from "./api/auth/[...nextauth]/options";
 
 export default async function Home() {
   const tasks = await usePrisma.task.findMany();
 
+  const session = await getServerSession(options);
+  console.log("Session: ", session);
+
   return (
-    <main className="">
-      <h1>Tasks.</h1>
-      <TaskList tasks={tasks} />
+    <main className="min-h-screen flex justify-center">
+      <div className="container">
+        <h1>Tasks.</h1>
+        <TaskList tasks={tasks} />
+      </div>
     </main>
   );
 }
